760486c5ca378478a7fb348c3a6a7e3c67f1ae24
Sirio Balmelli <sirio@b-ad.ch>
2020-07-10 02:08:58 +0200

    temp repo development stub

--- pkgs/development/tools/misc/nrfutil/default.nix
+++ pkgs/development/tools/misc/nrfutil/default.nix
@@ -5,11 +5,15 @@ with python37Packages; buildPythonApplication rec {
   version = "6.1";
 
   src = fetchFromGitHub {
-    owner = "NordicSemiconductor";
     repo = "pc-nrfutil";
-    rev = "v${version}";
-    sha256 = "0g43lf5jmk0qxb7r4h68wr38fli6pjjk67w8l2cpdm9rd8jz4lpn";
+    #rev = "v${version}";
+    #owner = "NordicSemiconductor";
+    #sha256 = "0g43lf5jmk0qxb7r4h68wr38fli6pjjk67w8l2cpdm9rd8jz4lpn";
+    rev = "master";
+    owner = "siriobalmelli-foss";
+    sha256 = "0sqik1g74ghpn8b2sh7mzlwf796l95pzshgb51ihsil00mv5nyd6";
   };
+  #src = ~/repos/foss/pc-nrfutil;
 
   propagatedBuildInputs = [ pc-ble-driver-py six pyserial enum34  click ecdsa
     protobuf tqdm piccata pyspinel intelhex pyyaml crcmod libusb1 ipaddress ];
