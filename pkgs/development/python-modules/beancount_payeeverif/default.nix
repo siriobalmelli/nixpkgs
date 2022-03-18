0ccf0c2aefc97df29b4858eda06befb0f1864ff6
Sirio Balmelli <sirio@b-ad.ch>
2022-03-18 13:45:58 +0100

    python3Packages.beancount_payeeverif: init at 1.0.2
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

---
+++ pkgs/development/python-modules/beancount_payeeverif/default.nix
@@ -0,0 +1,60 @@
+{ lib, buildPythonPackage, fetchPypi, isPy3k
+, setuptools-scm
+, beancount
+, pytest, sh
+}:
+
+buildPythonPackage rec {
+  version = "1.0.2";
+  pname = "beancount_payeeverif";
+
+  disabled = !isPy3k;
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "03gf07797nbx4f6khapmkjz1mgbhsiqv0pmrzkicd2qyaba65mwk";
+  };
+
+  nativeBuildInputs = [
+    setuptools-scm
+  ];
+
+  propagatedBuildInputs = [
+    beancount
+  ];
+
+  checkInputs = [
+    pytest
+    sh
+  ];
+
+  checkPhase = ''
+    pytest
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/siriobalmelli/beancount_payeeverif";
+    description = "Payee verification plugin for Beancount";
+    longDescription = ''
+      beancount_payeeverif is the "payee verification" plugin for beancount,
+      fulfilling the following functions:
+
+      Every transaction has a non-NULL payee field:
+
+      ; this will throw an error
+      2020-06-01  *   ""   "fix faucet leak"
+        Expenses:General
+        Assets:Bank -150 bean
+
+      ; this will pass validation
+      2020-06-02  *   "plumber"   "fix faucet leak"
+        Expenses:General
+        Assets:Bank -150 bean
+
+      *TODO: coming soon* Transactions touching certain accounts
+      must match an allowed_payees regex.
+    '';
+    license = licenses.mit;
+    maintainers = with maintainers; [ siriobalmelli ];
+  };
+}
