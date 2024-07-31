2cc2c890b1793a87d4d1539aead4f6337d885c48
Sirio Balmelli <sirio@b-ad.ch>
2024-07-31 16:21:09 +0200

    python3Packages.beancount_payeeverif: init at 1.0.2
    Signed-off-by: Sirio Balmelli <sirio@b-ad.ch>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1716,6 +1716,8 @@ self: super: with self; {
 
   beanquery = callPackage ../development/python-modules/beanquery { };
 
+  beancount_payeeverif = callPackage ../development/python-modules/beancount_payeeverif { };
+
   beanstalkc = callPackage ../development/python-modules/beanstalkc { };
 
   beartype = callPackage ../development/python-modules/beartype { };
